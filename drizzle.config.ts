f2a5dfbc8a47eda2af9fbbba97c07b7f5c8f9791
Femi <ajuwonfemi121@gmail.com>
2026-09-01 14:44:05 +0100

    FEAT: config vitest for server-actions test, formatting

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'drizzle-kit';
+import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: './src/database/schemas', // Path to your schema file
-  out: './drizzle',             // Where migrations will be saved
-  dialect: 'postgresql',        // Tells drizzle to use Postgres
+  schema: "./src/database/schemas", // Path to your schema file
+  out: "./drizzle", // Where migrations will be saved
+  dialect: "postgresql", // Tells drizzle to use Postgres
   dbCredentials: {
     url: process.env.DATABASE_URL!, // Your Postgres connection string
   },
